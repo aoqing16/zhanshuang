ec5ea8919486e7780928d23f4213fd8ac6b49b3d
aoqing16 <wangchuanbing027@gmail.com>
2026-06-04 19:52:11 +0800

    修复了章节页滑动死循环的问题，解决点击不合法问题，同时改正了实际应使用的检测函数名

--- Python文件/config.py
+++ Python文件/config.py
@@ -13,6 +13,7 @@ print(f'项目根目录路径：{项目根目录路径}')
 章节标签roi=[0,694,404,760]
 章节黑名单 = {
     "升格-苦行之旅": os.path.join(项目根目录路径, "ziyuanwenjian",'biaoshi','img_20.png'),
+    "升格-终末预览": os.path.join(项目根目录路径, "ziyuanwenjian",'biaoshi','img_22.png'),
 }
 章节roi = [556, 956, 1943, 1158]
 print(章节标签黑名单)
